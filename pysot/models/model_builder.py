e1621e52a90feb2612ec24401964062aacee0514
Nguyễn Thị Thanh Tuyền <22521632@gm.uit.edu.vn>
2025-11-07 17:59:07 +0700

    model_builder

--- pysot/models/model_builder.py
+++ pysot/models/model_builder.py
@@ -1,10 +1,8 @@
 # Copyright (c) SenseTime. All Rights Reserved.
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
+from __future__ import absolute_import, division, print_function, unicode_literals
 
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -59,28 +57,33 @@ class ModelBuilder(nn.Module):
         if cfg.MASK.MASK:
             mask, self.mask_corr_feature = self.mask_head(self.zf, xf)
         return {
-                'cls': cls,
-                'loc': loc,
-                'mask': mask if cfg.MASK.MASK else None
-               }
+            'cls': cls,
+            'loc': loc,
+            'mask': mask if cfg.MASK.MASK else None
+        }
 
     def mask_refine(self, pos):
         return self.refine_head(self.xf, self.mask_corr_feature, pos)
 
     def log_softmax(self, cls):
         b, a2, h, w = cls.size()
-        cls = cls.view(b, 2, a2//2, h, w)
+        cls = cls.view(b, 2, a2 // 2, h, w)
         cls = cls.permute(0, 2, 3, 4, 1).contiguous()
         cls = F.log_softmax(cls, dim=4)
         return cls
-    
+
     def forward(self, data):
-        """ only used in training — hỗ trợ nhiều ảnh template """
-        templates = data['templates'].cuda()
-        search = data['search'].cuda()
-        label_cls = data['label_cls'].cuda()
-        label_loc = data['label_loc'].cuda()
-        label_loc_weight = data['label_loc_weight'].cuda()
+        """Only used in training — hỗ trợ nhiều ảnh template"""
+
+        # Lấy device của model, CPU hoặc GPU
+        device = next(self.parameters()).device
+
+        # Chuyển dữ liệu sang device
+        templates = data['templates'].to(device)
+        search = data['search'].to(device)
+        label_cls = data['label_cls'].to(device)
+        label_loc = data['label_loc'].to(device)
+        label_loc_weight = data['label_loc_weight'].to(device)
 
         B, N, C, H, W = templates.shape
         templates_flat = templates.view(B * N, C, H, W)
@@ -118,50 +121,8 @@ class ModelBuilder(nn.Module):
 
         if cfg.MASK.MASK:
             mask, self.mask_corr_feature = self.mask_head(zf, xf)
-            mask_loss = None
+            mask_loss = None  # hoặc tính mask_loss nếu có
             outputs['total_loss'] += cfg.TRAIN.MASK_WEIGHT * (mask_loss or 0)
             outputs['mask_loss'] = mask_loss
 
         return outputs
-
-
-    # def forward(self, data):
-    #     """ only used in training
-    #     """
-    #     template = data['template'].cuda()
-    #     search = data['search'].cuda()
-    #     label_cls = data['label_cls'].cuda()
-    #     label_loc = data['label_loc'].cuda()
-    #     label_loc_weight = data['label_loc_weight'].cuda()
-
-    #     # get feature
-    #     zf = self.backbone(template)
-    #     xf = self.backbone(search)
-    #     if cfg.MASK.MASK:
-    #         zf = zf[-1]
-    #         self.xf_refine = xf[:-1]
-    #         xf = xf[-1]
-    #     if cfg.ADJUST.ADJUST:
-    #         zf = self.neck(zf)
-    #         xf = self.neck(xf)
-    #     cls, loc = self.rpn_head(zf, xf)
-
-    #     # get loss
-    #     cls = self.log_softmax(cls)
-    #     cls_loss = select_cross_entropy_loss(cls, label_cls)
-    #     loc_loss = weight_l1_loss(loc, label_loc, label_loc_weight)
-
-    #     outputs = {}
-    #     outputs['total_loss'] = cfg.TRAIN.CLS_WEIGHT * cls_loss + \
-    #         cfg.TRAIN.LOC_WEIGHT * loc_loss
-    #     outputs['cls_loss'] = cls_loss
-    #     outputs['loc_loss'] = loc_loss
-
-    #     if cfg.MASK.MASK:
-    #         # TODO
-    #         mask, self.mask_corr_feature = self.mask_head(zf, xf)
-    #         mask_loss = None
-    #         outputs['total_loss'] += cfg.TRAIN.MASK_WEIGHT * mask_loss
-    #         outputs['mask_loss'] = mask_loss
-    #     return outputs
-
